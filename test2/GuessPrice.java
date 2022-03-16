f06d90cd985ce592a9660260940f2a20af90cd94
aguang45 <88522807+aguang45@users.noreply.github.com>
2022-03-16 09:08:23 +0800

    Add files via upload

--- test2/GuessPrice.java
+++ test2/GuessPrice.java
@@ -10,46 +10,50 @@ import java.util.Scanner;
 
 public class GuessPrice {
     public static void main(String[] args) {
-        System.out.println("欢迎来到猜价格游戏!");
+        System.out.println("\t***欢迎来到猜价格游戏!***");
         Random r = new Random();
         //创建Random对象
         int num = r.nextInt(899) + 100;
         //创建大小为100-999的随机数，保存在num
-        System.out.println("已生成随机价格！（100-999）");
-        int i = 0;
-        //i用于记录游戏循环次数
+        System.out.println("\t已生成随机价格！（100-999）");
+        int i = 0, max = 999, min = 100;
+        //i用于记录游戏循环次数,max、min分别记录最大值最小值，用于输入错误时给出范围提示
 
         Scanner sc = new Scanner(System.in);
         while (true) {
             //永真循环用于输入失败继续进行操作
 
-            System.out.println("\n请输入你猜的价格:(输入0停止游戏):");
-            int a = sc.nextInt();
-            //键盘录入数据存放在a
-
             i++;
             //累加记录输入次数
 
-            if (a >= 100 && a < 1000)
+            System.out.println("\n"+i+"、请输入你猜的价格(输入0停止游戏):");
+            int a = sc.nextInt();
+            //键盘录入数据存放在a
+
+            if (a >= 100 && a < 1000) {
 
                 //按照输入数据大小给出相应的提示
+                if (a > num) {
+                    if (a < max)
+                        max = a;
+                    System.out.println("\t你输入的价格大了!答案的范围在: " + min + "-" + max+" !");
 
-                if (a > num)
-                    System.out.println("你输入的价格大了!");
-                else if (a < num)
-                    System.out.println("你输入的价格小了!");
-                else {
-                    System.out.println("恭喜你猜对了，本次游戏的随机价格为:" + num + "\n本次游戏你共输入了" + i + "次");
+                } else if (a < num) {
+                    if (a > min)
+                        min = a;
+                    System.out.println("\t你输入的价格小了!答案的范围在: " + min + "-" + max+" !");
+                } else {
+                    System.out.println("\n\n\t\t\t***恭喜你猜对了***\n\t本次游戏的随机价格为:" + num + ",你共输入了" + i + "次");
                     return;
                     //猜对执行，return，跳出程序
                 }
-            else if(a == 0) {
-                System.out.println("本次游戏已停止，你共输入了" + i +"次");
-                return;
             }
-
-            else
-                System.out.println("请正确输入100-999的整数！");
+            else if (a == 0) {
+                    System.out.println("本次游戏已停止，你共输入了" + i + "次");
+                    return;
+                }
+             else
+                System.out.println("请正确输入"+min+" - "+max+"的整数！");
         }
     }
 }
